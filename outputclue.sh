d1422af79d2be939ff9a1e182acf649346371c37
MadMike232 <71754668+MadMike232@users.noreply.github.com>
2020-09-23 20:13:20 +0200

    Update outputclue.sh

--- outputclue.sh
+++ outputclue.sh
@@ -6,7 +6,8 @@ fi
 
 file=$1
 bug=7c85d987a917c2a555d1391426978f05
-mesg="Level 7: \n Robert Morris has been here...\nI love messing with these amateur programmers!!\nIf you want some real fun, then you should try resolving a conflict between this branch (tree) and code4life.\nI introduced a little bug that you should fix in the conflict. >:)\nAfter you merge these 2 files you should run the shell script again!!\n\nGood Luck!!!\n\n Hint: https://help.github.com/articles/resolving-a-merge-conflict-from-the-command-line/ "
+string="e  oy  a    e  io   o   o "
+mesg="Level 7: \n Robert Morris has been here...\nI love messing with these amateur programmers!!\nIf you want some real fun, then you should try finding the little bug and fix the script. >:)\nAfter that run again the shell script!!\n\nGood Luck!!!\n\n "
 merges=$(git log --format=%h --merges | head -1)
 csum="md5sum"
 if [ $(echo "$OSTYPE" | grep darwin) ];then
@@ -23,7 +24,7 @@ if [ "$file" = "nextclue_input.cpp" ];then
         fi; 
       done;
     done < $file ;
-    echo -e "Well, congratulations!! You fixed my conflict!!\nIf you would like to continue, then you should checkout to the $(echo 90mP8ouQHsNe | tr -d '0-9A-Z') branch!!\n" ;
+    echo -e "Well, congratulations!! You fixed the bug!!\nIf you would like to continue, then you should checkout to the $(echo OTBtUDhvdVFIc05lCg== | base64 --decode | tr -d '0-9A-Z') branch!!\nAnd please keep the following vowels in mind \"$string\"\n ";
    else 
      echo -e $mesg; 
      exit;
